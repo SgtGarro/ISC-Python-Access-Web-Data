fa33778adf10fc10a95d48c92b4550dc7da7319b
SgtGarro <marcelogarro137@gmail.com>
2022-11-20 13:21:57 -0500

    Chapter 9

---
+++ chapter9/main.py
@@ -0,0 +1,10 @@
+# stuff = dict()
+# print(stuff['candy'])
+
+stuff = dict()
+print(stuff.get('candy',-1))
+
+# if key in counts:
+#     counts[key] = counts[key] + 1
+# else:
+#     counts[key] = 1
